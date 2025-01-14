793a8d92e9541643bbc894c1c53da58ae2a1fa64
Ricardo Q. <ricardoqg41@icloud.com>
2025-01-14 18:39:40 -0500

    add getMovieTrailers

--- src/services/api.js
+++ src/services/api.js
@@ -10,3 +10,9 @@ export const searchMovies = async (query) => {
     const data = await response.json();
     return data.results;
 }
+
+export const getMovieTrailers = async (movieId) => {
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/movie/${movieId}/videos?api_key=${import.meta.env.VITE_API_KEY}`);
+    const data = await response.json();
+    return data.results.find(video => video.type === "Trailer" && video.site === "YouTube");
+}
