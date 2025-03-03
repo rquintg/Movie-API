e4e51b69d7d3048e9982085567e896cb8d92dee8
Ricardo Q. <ricardoqg41@icloud.com>
2025-03-03 00:35:12 -0500

    reformat code

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,30 +3,30 @@ import {useMovieContext} from "../contexts/MovieContext.jsx";
 import '../css/MovieCard.css'
 
 
-function MovieCard({movie, onShowModalInfo}){
+function MovieCard({movie, onShowModalInfo}) {
     const {addFavorites, removeFromFavorites, isFavorite} = useMovieContext();
     const favorite = isFavorite(movie.id);
 
 
-    function onFavoriteClick(e){
+    function onFavoriteClick(e) {
         e.preventDefault();
-        if(favorite) removeFromFavorites(movie.id);
+        if (favorite) removeFromFavorites(movie.id);
         else addFavorites(movie);
     }
 
-    function truncateOverview(overview, limit){
-        if(overview.length > limit){
+    function truncateOverview(overview, limit) {
+        if (overview.length > limit) {
             return overview.substring(0, limit) + "...";
         }
         return overview;
     }
 
-    return(
+    return (
         <div className="movie-card">
             <div className="movie-poster">
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
                 <div className="movie-ocverlay">
-                    <button className={`favorite-btn ${favorite ? "active" : "" }`} onClick={onFavoriteClick}>
+                    <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
                         <i className="bi bi-star-fill"></i>
                     </button>
                 </div>
@@ -39,7 +39,9 @@ function MovieCard({movie, onShowModalInfo}){
                 <p>{truncateOverview(movie.overview, 100)}</p>
                 <p>Release date: <b>{movie.release_date?.split("-")[0]}</b></p>
 
-                <button className="btn text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3" onClick={onShowModalInfo}>Show Details</button>
+                <button className="btn text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3"
+                        onClick={onShowModalInfo}>Show Details
+                </button>
 
             </div>
 
